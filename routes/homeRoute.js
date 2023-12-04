fb8ae2da1158726fb724fb793fa3cbf51855c90f
Elisa McGraw <elisamasiero.em@gmail.com>
2023-12-03 23:06:01 -0500

    comming changes for recipe feature

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,15 +1,24 @@
 import express from "express";
 const router = express.Router();
 
-router.route("/").get(async (req, res) => {
-  //code here for GET will render the home handlebars file
-
+router.route("/recipes").get(async (req, res) => {
+  //code here for GET will render the inventory handlebars file
   if (req.session.user) {
+    res.render("recipes", { name: req.session.user.name });
+  } else {
+    res.redirect("../");
+  }
+});
+
+router.route("/").get(async (req, res) => {
+  // Only redirect to '/food' if the user is on the home page ('/')
+  if (req.session.user && req.originalUrl === '/') {
     res.redirect("/food");
   } else {
     res.render("home", {});
   }
 });
+
 
 router.route("/about").get(async (req, res) => {
   //code here for GET will render the about handlebars file
@@ -28,6 +37,7 @@ router.route("/food").get(async (req, res) => {
     res.redirect("../");
   }
 });
+
 
 router.route("/analytics").get(async (req, res) => {
   //code here for GET will render the inventory handlebars file
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,11 +1,45 @@
-import express from "express";
+import express from 'express';
+import { getUserInventory } from '../data/inventory.js'; // Adjust import based on your project structure
+
 const router = express.Router();
 
-// Example route for fetching recipes
-router.get("/recipes", (req, res) => {
-  const ingredients = req.query.ingredients;
-  // Use the ingredients to fetch recipes from database or an external API
-  // Render the recipes using Handlebars
+router.get('/recipes', async (req, res) => {
+    if (!req.user) {
+        return res.redirect('/login');
+    }
+
+    try {
+        const inventoryItems = await getUserInventory(req.user.id);
+        let hasIngredients = inventoryItems && inventoryItems.length > 0;
+
+        res.render('recipes', {
+            layout: 'main',
+            name: req.user.name,
+            hasIngredients,
+            inventoryItems  // Pass inventory items to the template
+        });
+    } catch (error) {
+        console.error('Error in /recipes route:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
+
+
 export default router;
+
+
+/*
+// Additional route to handle fetching recipes by ingredient
+router.get('/fetch-recipes', logoutMiddleware, async (req, res) => {
+    try {
+        const ingredient = req.query.ingredient;
+        console.log('Fetching recipes for ingredient:', ingredient);
+        const recipes = await findRecipesByIngredients([ingredient]);
+        res.json(recipes);
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+        res.status(500).json({ message: 'Error fetching recipes' });
+    }
+});
+*/
