03fc5fc4fb60f0c99bc9fae1121733e80a19359f
Joscandy <jos.candy@gmail.com>
2023-12-09 15:42:13 -0500

    corrected recipe calls

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -1,45 +1,72 @@
 // This script will run on the client side, handling clicks on ingredient buttons
 // and fetching recipe information based on the clicked ingredient.
 
-document.addEventListener('DOMContentLoaded', () => {
-    const ingredientLinks = document.querySelectorAll('.ingredient-link');
-    const recipesContainer = document.getElementById('recipes-container');
+document.addEventListener("DOMContentLoaded", () => {
+  const ingredientLinks = document.querySelectorAll(".ingredient-link");
+  const recipesContainer = document.getElementById("recipes-container");
 
-    ingredientLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const ingredient = e.target.getAttribute('data-ingredient');
+  ingredientLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const ingredient = e.target.getAttribute("data-ingredient");
 
-            fetch(`/recipes/fetch-recipes?ingredient=${encodeURIComponent(ingredient)}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(recipes => {
-                    displayRecipes(recipes);
-                })
-                .catch(error => {
-                    console.error('Error fetching recipes:', error);
-                    displayErrorMessage('Error fetching recipes. Please try again later.');
-                });
+      fetch(
+        `/recipes/fetch-recipes?ingredient=${encodeURIComponent(ingredient)}`
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((recipes) => {
+          collectAdditionalRecipeInfo(recipes);
+        })
+        .catch((error) => {
+          console.error("Error fetching recipes:", error);
+          displayErrorMessage(
+            "Error fetching recipes. Please try again later."
+          );
         });
     });
-    
-    function displayRecipes(recipes) {
-        recipesContainer.innerHTML = ''; // Clear previous results
+  });
 
-        if (!Array.isArray(recipes) || recipes.length === 0) {
-            const message = document.createElement('p');
-            message.className = 'no-recipes-message';
-            message.textContent = 'No recipes found for this ingredient.';
-            recipesContainer.appendChild(message);
-        } else {
-            recipes.forEach(recipe => {
-                const recipeElement = document.createElement('div');
-                recipeElement.className = 'recipe';
-                recipeElement.innerHTML = `
+  function collectAdditionalRecipeInfo(recipes) {
+    Promise.all(
+      recipes.map((recipe) =>
+        fetch(`/recipes/recipe-info/${encodeURIComponent(recipe.id)}`).then(
+          (response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          }
+        )
+      )
+    )
+      .then((recipeInfoArray) => {
+        console.log("recipeInfo: ", recipeInfoArray.length);
+        displayRecipes(recipeInfoArray);
+      })
+      .catch((error) => {
+        console.error("Error fetching recipes:", error);
+        displayErrorMessage("Error fetching recipes. Please try again later.");
+      });
+  }
+
+  function displayRecipes(recipes) {
+    recipesContainer.innerHTML = ""; // Clear previous results
+
+    if (!Array.isArray(recipes) || recipes.length === 0) {
+      const message = document.createElement("p");
+      message.className = "no-recipes-message";
+      message.textContent = "No recipes found for this ingredient.";
+      recipesContainer.appendChild(message);
+    } else {
+      recipes.forEach((recipe) => {
+        const recipeElement = document.createElement("div");
+        recipeElement.className = "recipe";
+        recipeElement.innerHTML = `
                     <h3><a href="${recipe.spoonacularSourceUrl}" target="_blank">${recipe.title}</a></h3>
                     <a href="${recipe.spoonacularSourceUrl}" target="_blank">
                         <img src="${recipe.image}" alt="${recipe.title}">
@@ -48,18 +75,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     <p>Ready in: ${recipe.readyInMinutes} minutes</p>
                     <p>Recipe from: <a href="${recipe.sourceUrl}" target="_blank">${recipe.sourceName}</a></p>
                 `;
-                recipesContainer.appendChild(recipeElement);
-            });
-        }
-    
-        
+        recipesContainer.appendChild(recipeElement);
+      });
     }
-    function displayErrorMessage(message) {
-        recipesContainer.innerHTML = `<p class="error-message">${message}</p>`;
-    }
-
-    
+  }
+  function displayErrorMessage(message) {
+    recipesContainer.innerHTML = `<p class="error-message">${message}</p>`;
+  }
 });
-
-
-
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,75 +1,63 @@
-import express from 'express';
-import foodsData from '../data/foods.js';
-import { findRecipesByIngredients } from '../data/recipes.js'; 
-import axios from 'axios';
+import express from "express";
+import foodsData from "../data/foods.js";
+import { findRecipesByIngredients } from "../data/recipes.js";
+import axios from "axios";
 
 const router = express.Router();
-const SPOONACULAR_API_KEY = 'd4ba958eb33242358a0aa3588e425c68';
+const SPOONACULAR_API_KEY = "d4ba958eb33242358a0aa3588e425c68";
 
-router.get('/', async (req, res) => { 
-    console.log('Accessing /recipes route');
-    
-    if (!req.session || !req.session.user) {
-        // If not authenticated, redirect to login
-        return res.redirect('/login');
-    }
-    console.log(req.session.user.id);
-    try {
-        const userId = req.session.user.id; 
-        const inventoryItems = await foodsData.getFoodByUserId(userId);
-        let hasIngredients = inventoryItems && inventoryItems.length > 0;
+router.get("/", async (req, res) => {
+  console.log("Accessing /recipes route");
 
-        console.log('Inventory Items:', inventoryItems);
-        console.log('Has Ingredients:', hasIngredients);
-        
+  if (!req.session || !req.session.user) {
+    // If not authenticated, redirect to login
+    return res.redirect("/login");
+  }
+  try {
+    const userId = req.session.user.id;
+    const inventoryItems = await foodsData.getFoodByUserId(userId);
+    let hasIngredients = inventoryItems && inventoryItems.length > 0;
 
-        res.render('recipes', {
-            layout: 'main',
-            name: req.session.user.name, 
-            hasIngredients,
-            inventoryItems
-    
-        });
-    } catch (error) {
-        console.error('Error in /recipes route:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    res.render("recipes", {
+      layout: "main",
+      name: req.session.user.name,
+      hasIngredients,
+      inventoryItems,
+    });
+  } catch (error) {
+    console.error("Error in /recipes route:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-router.get('/fetch-recipes', async (req, res) => {
-    try {
-        const ingredient = req.query.ingredient;
-        const recipes = await findRecipesByIngredients([ingredient]);
-        res.json(recipes);
-    } catch (error) {
-        console.error('Error fetching recipes:', error);
-        res.status(500).json({ message: 'Error fetching recipes' });
-    }
+router.get("/fetch-recipes", async (req, res) => {
+  try {
+    const ingredient = req.query.ingredient;
+    const recipes = await findRecipesByIngredients([ingredient]);
+    res.json(recipes);
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    res.status(500).json({ message: "Error fetching recipes" });
+  }
 });
 
+router.get("/recipe-info/:id", async (req, res) => {
+  const recipeId = req.params.id;
+  //   console.log(`Backend: Received request for recipe ID: ${recipeId}`);
 
-router.get('/recipe-info/:id', async (req, res) => {
-    const recipeId = req.params.id;
-    console.log(`Backend: Received request for recipe ID: ${recipeId}`);
+  if (!recipeId) {
+    return res.status(400).json({ error: "No recipe ID provided" });
+  }
 
-    if (!recipeId) {
-        return res.status(400).json({ error: 'No recipe ID provided' });
-    }
+  const url = `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=${SPOONACULAR_API_KEY}`;
 
-    const url = `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=${SPOONACULAR_API_KEY}`;
-
-    try {
-        const response = await axios.get(url);
-        console.log('Backend: Sending recipe details:', response.data);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Backend: Error fetching recipe details:', error);
-        res.status(500).json({ error: 'Error fetching recipe details' });
-    }
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Backend: Error fetching recipe details:", error);
+    res.status(500).json({ error: "Error fetching recipe details" });
+  }
 });
-
-
 
 export default router;
-
-
